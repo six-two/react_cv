f02bd973e3591c6d9638506d5466679191e2f1f3
six-two <git@six-two.dev>
2020-12-11 18:05:02 +1300

    Made run.sh execute yaml2json. Created 2 different versions of yaml2json.

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 # Change directories to the directory that contains this file
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+# Create / update the language translation files
+python3 data/yaml2json.py
+
 # Start the app / do something else
 if [ "$1" == "deploy" ]
 then
